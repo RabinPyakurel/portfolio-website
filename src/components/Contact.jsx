74148c620d66cf595906ea493232999f3b2fae45
Rabin Pyakurel <rabinstar137@gmail.com>
2024-12-22 22:08:41 +0545

    added working mail link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
             <div className='text-center tracking-tighter'>
                 <p className='my-4'>{CONTACT.address}</p>
                 <p className='my-4'>{CONTACT.phoneNo}</p>
-                <a href="" className='border-b'>{CONTACT.email}</a>
+                <a href="mailto:contact@rabinpyakurel.com.np" className='border-b'>{CONTACT.email}</a>
             </div>
         </div>
     )
